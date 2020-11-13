fa0e44641bd92fdd42ade41561175debfbc113d5
Kurt S Steffan <kurt.s.steffan@gmail.com>
2020-11-13 14:35:59 +0000

    Added check in guest to room test

--- classes/room.py
+++ classes/room.py
@@ -2,5 +2,7 @@ class Room:
     def __init__( self, name ):
         self.name = name
         self.song_list = []
+        self.guest_list = []
     
-    
+    def check_in_guest( self, guest_name):
+        self.guest_list.append(guest_name)
--- tests/room_test.py
+++ tests/room_test.py
@@ -1,10 +1,18 @@
 import unittest
 from classes.room import Room
+from classes.guest import Guest
 
 
 class TestRoom(unittest.TestCase):
     def setUp(self):
         self.room1 = Room("Room 1")
+        self.room2 = Room("Room 2")
 
     def test_create_room( self ):
         self.assertEqual("Room 1", self.room1.name)
+
+    def test_check_in_guest( self ):
+        guest = Guest("Sam")
+        self.room1.check_in_guest( guest.name )
+        self.assertEqual( 1, len(self.room1.guest_list))
+       
