69072501e937e73c2827af2197a2bd9566340ed1
Kurt S Steffan <kurt.s.steffan@gmail.com>
2020-11-13 14:01:54 +0000

    Added test to check song can be created

--- tests/song_test.py
+++ tests/song_test.py
@@ -4,4 +4,7 @@ from classes.song import Song
 
 class TestSong(unittest.TestCase):
     def setUp(self):
-        pass
+        self.song1 = Song("People Like Us")
+
+    def test_song_exists( self ):
+        self.assertEqual("People Like Us", self.song1.name)
