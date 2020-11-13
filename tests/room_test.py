e41c3a591ab8a7473ca304194fc6edcbf7e06dcb
Kurt S Steffan <kurt.s.steffan@gmail.com>
2020-11-13 14:09:55 +0000

    Added test to check room can be created

--- tests/room_test.py
+++ tests/room_test.py
@@ -4,4 +4,7 @@ from classes.room import Room
 
 class TestRoom(unittest.TestCase):
     def setUp(self):
-        pass
+        self.room1 = Room("Room 1")
+
+    def test_create_room( self ):
+        self.assertEqual("Room 1", self.room1.name)
